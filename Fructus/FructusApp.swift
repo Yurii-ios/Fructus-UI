227117a9d6c115bbcc461ca288c813430a411b88
Yurii <Yurii>
2020-12-18 18:47:32 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Yurii Sameliuk on 18/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
