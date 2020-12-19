bc4d69d5d0d98931f8c3ebbd087a9cb3e18e71cf
Yurii <Yurii>
2020-12-19 23:05:22 +0100

    create FruitRowView

---
+++ Fructus/View/FruitRowView.swift
@@ -0,0 +1,26 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Yurii Sameliuk on 19/12/2020.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//MARK: - PREVIEW
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
