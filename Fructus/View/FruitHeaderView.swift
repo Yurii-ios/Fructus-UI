0a6180658456d955cf63b811aee84041cb92e950
Yurii <Yurii>
2020-12-20 22:02:29 +0100

    add header

---
+++ Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Yurii Sameliuk on 20/12/2020.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    //MARK: - PROPERTIES
+    var fruit: Fruit
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            // add a new gradient color as an individual view for the bottom layer of the stack
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            //  IMAGE
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.50), radius: 8, x: 6, y: 8)
+                // for iPad scren
+                .padding(.vertical, 20)
+        } //: ZSTACK
+        .frame(height: 440)
+    }
+}
+
+//MARK: - PREVIEW
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView(fruit: fruitsData[0])
+            .previewLayout(.fixed(width: 375, height: 440))
+    }
+}
