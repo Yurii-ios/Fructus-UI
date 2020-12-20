07f1ada6a6f67c328a6b6a2a4bb5145bc828de14
Yurii <Yurii>
2020-12-20 22:10:14 +0100

    add animations to header

--- Fructus/View/FruitHeaderView.swift
+++ Fructus/View/FruitHeaderView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct FruitHeaderView: View {
     //MARK: - PROPERTIES
     var fruit: Fruit
+    
+    @State private var isAnimatingImage: Bool = false
+    
     //MARK: - BODY
     var body: some View {
         ZStack {
@@ -22,8 +25,16 @@ struct FruitHeaderView: View {
                 .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.50), radius: 8, x: 6, y: 8)
                 // for iPad scren
                 .padding(.vertical, 20)
+                .scaleEffect(isAnimatingImage ? 1.0 : 0.6)
         } //: ZSTACK
         .frame(height: 440)
+        // ANIMATION
+        .onAppear() {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimatingImage = true
+                
+            }
+        }
     }
 }
 
