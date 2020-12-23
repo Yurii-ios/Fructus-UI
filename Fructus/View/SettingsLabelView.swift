75c9fafd293d2a1e5a91348168c4a2f24ddd17dc
Yurii <Yurii>
2020-12-23 21:52:22 +0100

    create SettingsLabelView

---
+++ Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by Yurii Sameliuk on 23/12/2020.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    //MARK: - PROPERTIES
+    var labelText: String
+    var labelImage: String
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+    Spacer()
+    Image(systemName: labelImage)
+        } //: HSTACK
+    }
+}
+
+//MARK: - PREVIEW
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
